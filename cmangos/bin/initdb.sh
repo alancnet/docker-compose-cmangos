21419c228423a5746fefb993b3638a5f4ca183ca
Alan Colon <github@alanc.net>
2016-06-22 09:27:01 -0600

    Optimizing container size

--- cmangos/bin/initdb.sh
+++ cmangos/bin/initdb.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
+apt-get update && \
+apt-get install -y mysql-client git-core && \
 wait-for-mysql.sh && \
+git clone $GITHUB_CMANGOS mangos
+git clone $GITHUB_ACID acid && \
+git clone $GITHUB_DATABASE classicdb && \
+cp /InstallFullDB.sh /classicdb/ && \
 mysql -uroot -h $MYSQL_HOST -P $MYSQL_PORT -p$MYSQL_ROOT_PASSWORD < /mangos/sql/create/db_create_mysql.sql && \
 mysql -uroot -h $MYSQL_HOST -P $MYSQL_PORT -p$MYSQL_ROOT_PASSWORD mangos < /mangos/sql/base/mangos.sql && \
 mysql -uroot -h $MYSQL_HOST -P $MYSQL_PORT -p$MYSQL_ROOT_PASSWORD characters < /mangos/sql/base/characters.sql && \
@@ -7,4 +13,7 @@ mysql -uroot -h $MYSQL_HOST -P $MYSQL_PORT -p$MYSQL_ROOT_PASSWORD realmd < /mang
 pushd /classicdb && \
 ./InstallFullDB.sh && \
 popd && \
+apt-get remove -y mysql-client git-core && \
+apt-get clean && \
+rm -rf /var/lib/apt/lists/* /tmp/* /var/tmp/ /acid /classicdb /mangos
 echo Done.
