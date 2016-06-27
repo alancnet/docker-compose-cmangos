424af558850a0e32594cfa06c74f755a8ad067e1
Alan Colon <github@alanc.net>
2016-06-26 22:51:24 -0600

    Extras

--- cmangos/bin/mangosd.sh
+++ cmangos/bin/mangosd.sh
@@ -2,4 +2,12 @@
 wait-for-mysql.sh
 echo "# Append commands to this file to inject them into the mangosd service">>/commands
 cd /runtime/bin
-tail -n 0 -f /commands | ./mangosd
+touch /log
+function clearLog {
+  while true; do
+    echo>/log
+    sleep 600
+  done
+}
+clearLog&
+tail -n 0 -f /commands | ./mangosd | tee /log
